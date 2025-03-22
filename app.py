fe27e08c07a190ae4dfb10e14268917417995ab1
nbdigitech <31731388+nbdigitech@users.noreply.github.com>
2025-03-22 14:16:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -476,8 +476,8 @@ import zipfile
 
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": ["https://cmo-front-beta.vercel.app",
-    "https://cmo-front-beta-nb-digital-technologies-projects.vercel.app"]}}, supports_credentials=True)
+CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
+
 
 # ✅ Ensure OPTIONS requests are handled correctly
 @app.before_request
